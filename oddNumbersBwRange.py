611c68ae8075f6779e0305c5057210a3bfb4d0e6
NeslynChellaiah <36042097+NeslynChellaiah@users.noreply.github.com>
2019-03-18 21:33:41 +0530

    Create oddNumbersBwRange.py

---
+++ oddNumbersBwRange.py
@@ -0,0 +1,10 @@
+class main():
+    def func(self,a,b):
+       for i in range (a,b):
+            if (i%2!=0):
+                print(i,end=" ")
+ob = main()
+a,b = input().split()
+a = int(a)+1
+b = int(b)
+ob.func(a,b)
